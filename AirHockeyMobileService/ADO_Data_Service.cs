a97f1c142690cc686edb226154a7a9727f58d434
Oren Afek <oren.afek@campus.technion.ac.il>
2015-11-16 16:18:03 +0200

    Added an updateUser method to ADO_Data_Service Class

--- AirHockeyMobileService/ADO_Data_Service.cs
+++ AirHockeyMobileService/ADO_Data_Service.cs
@@ -10,7 +10,6 @@ namespace AirHockeyMobileService
 {
     public class ADO_Data_Service
     {
-
         private static SqlConnection _conn;
         private static SqlTransaction _trans;
         private static SqlDataAdapter _adapter;
@@ -24,35 +23,54 @@ namespace AirHockeyMobileService
             _conn = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["MS_TableConnectionString"].ConnectionString);
             _cmd = new SqlCommand();
             _cmd.Connection = _conn;
-       
+            _ds = new DataSet();
+
         }
         public static DataView getView(string viewName)
         {
             try
             {
-                _conn.Open();
+                _cmd = _conn.CreateCommand();
                 _cmd.CommandText = "View" + viewName;
+                _cmd.CommandType = CommandType.StoredProcedure;
                 _adapter = new SqlDataAdapter(_cmd);
                 _adapter.Fill(_ds);
                 return new DataView(_ds.Tables[0]);
             }
 
-            catch(SqlException e)
+            catch (SqlException e)
             {
                 return null;
             }
 
+            return null;
+
+        }
+    
+
+    public static void updateUser(int userID)
+    {
+            _cmd = _conn.CreateCommand();
+            _cmd.CommandText = "UPDATE Users SET id='a' AND Pass='999' WHERE id='77'";
+
+            try
+            {
+                _conn.Open();
+                _cmd.ExecuteNonQuery();
+                
+            }
+
+            catch(SqlException e)
+            {
+
+            }
+
             finally
             {
-                if (_conn.State == ConnectionState.Open)
+                if(_conn.State == ConnectionState.Open)
                 {
                     _conn.Close();
                 }
             }
-
-        }
-
-        
-
     }
 }
