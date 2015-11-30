a33ff1e5469b78c4bdd7ca1f6cbfa3ac04706a9d
Amit <amitoha@campus.technion.ac.il>
2015-11-30 14:39:58 +0200

    moved app to another folder

---
+++ HockeyApp/StartGamePage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace HockeyApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class StartGamePage : Page
+    {
+        public StartGamePage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void ButtonClick(object sender, RoutedEventArgs e)
+        {
+            // if text is valid
+            //this.Frame.Navigate(typeof(MainMenu), nameInput.Text);
+            //else
+            Utils.Show("Invalid name, please enter again", new List<UICommand> { new UICommand("Close") });
+        
+
+
+            //greetingOutput.Text = "Hello, " + nameInput.Text + "! Press the button to start a game.";
+
+        }
+
+    }
+}
---
+++ HockeyApp/Utils.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Popups;
+
+namespace HockeyApp
+{
+    public class Utils
+    {
+        public static async void Show(string text, List<UICommand> commands, uint defaultCommandIndex = 0, uint cancelCommandIndex = 0)
+        {
+            var message = new MessageDialog(text);
+            foreach (UICommand command in commands)
+            {
+                message.Commands.Add(command);
+            }
+            message.DefaultCommandIndex = defaultCommandIndex;
+            message.CancelCommandIndex = cancelCommandIndex;
+            await message.ShowAsync();
+        }
+    }
+}
