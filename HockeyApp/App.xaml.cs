cc0b665b659518c1d837ad8dd89741e48e7a73a2
Oren Afek <oren.afek@campus.technion.ac.il>
2015-11-30 16:20:42 +0200

    Modified and actually created the main menu. a splash screen was added.

--- HockeyApp/App.xaml.cs
+++ HockeyApp/App.xaml.cs
@@ -72,7 +72,7 @@ namespace HockeyApp
                 // When the navigation stack isn't restored navigate to the first page,
                 // configuring the new page by passing required information as a navigation
                 // parameter
-                rootFrame.Navigate(typeof(StartGamePage), e.Arguments);
+                rootFrame.Navigate(typeof(MainMenu), e.Arguments);
             }
             // Ensure the current window is active
             Window.Current.Activate();
