a2ad80accc4c80354d2746293be705b9e2032042
Oren Afek <oren.afek@campus.technion.ac.il>
2015-11-30 18:43:00 +0200

    Problem with running

--- HockeyApp/StartGamePage.xaml.cs
+++ HockeyApp/StartGamePage.xaml.cs
@@ -1,18 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+﻿using System.Collections.Generic;
 using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -42,5 +31,9 @@ namespace HockeyApp
             }
         }
 
+        private void NameInput_OnTextChanging(TextBox sender, TextBoxTextChangingEventArgs args)
+        {
+            inputButton.IsEnabled = ((TextBox) sender).Text != string.Empty;
+        }
     }
 }
