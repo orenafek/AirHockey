275c41c8e620f2e6dfca61c90d909de499770d70
Oren Afek <oren.afek@campus.technion.ac.il>
2015-12-13 19:04:05 +0200

    Changed the Bluetooth code again, still not working. added the original Arduino code in a new project

--- HockeyApp/BTConnecting.xaml.cs
+++ HockeyApp/BTConnecting.xaml.cs
@@ -1,24 +1,15 @@
-﻿using System;
+﻿using BluetoothConnectionManager;
+using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Devices.Bluetooth;
 using Windows.Devices.Bluetooth.Rfcomm;
 using Windows.Devices.Enumeration;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-using BluetoothConnectionManager;
 using Windows.Networking.Proximity;
 using Windows.Networking.Sockets;
+using Windows.Storage.Streams;
 using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -29,6 +20,11 @@ namespace HockeyApp
     /// </summary>
     public sealed partial class BTConnecting : Page
     {
+        private DeviceInformationCollection devices = null;
+        private RfcommDeviceService service = null;
+        private StreamSocket socket = null;
+        public static DataWriter writer = null;
+
         public BTConnecting()
         {
             this.InitializeComponent();
@@ -49,8 +45,10 @@ namespace HockeyApp
         private ConnectionManager BTConnectionManager { get; set; }
         private async void Ts_led_OnToggled(object sender, RoutedEventArgs e)
         {
-            string command = (sender as ToggleSwitch).IsOn ? "OFF" : "ON";
-            await BTConnectionManager.SendCommand(command);
+            int isOn = (ts_led.IsOn) ? 1 : 2;
+            writer = new DataWriter(socket.OutputStream);
+            writer.WriteInt32(isOn);
+            await writer.StoreAsync();
         }
 
         private async void PairAppToArduino()
@@ -78,7 +76,8 @@ namespace HockeyApp
 
         private void Btn_Connect_OnClick(object sender, RoutedEventArgs e)
         {
-            PairAppToArduino();
+            ConnectToBoard();
+            
         }
 
         private RfcommDeviceService Service { get; set; }
@@ -113,6 +112,36 @@ namespace HockeyApp
             var selector = "HC-06";/*BluetoothDevice.GetDeviceSelector();*/
             var devices = await DeviceInformation.FindAllAsync(selector);
 
+        }
+
+        private async void ConnectToBoard()
+        {
+            var devices = await DeviceInformation.FindAllAsync();
+            if(devices.Count > 0)
+            {
+                foreach(var device in devices)
+                {
+                    if(device.Name == "HC-06")
+                    {
+                        try
+                        {
+                            service = await RfcommDeviceService.FromIdAsync(device.Id);
+                            socket = new StreamSocket();
+                            Status.Text = "Connected";
+                            await socket.ConnectAsync(service.ConnectionHostName,
+                                service.ConnectionServiceName);
+                            ts_led.IsEnabled = true;
+                        }
+
+                        catch (Exception ex) { }
+                    }
+                }
+            }
+
+            else
+            {
+                Utils.Show(new MessageDialog("No Devices Were Found :("),null);
+            }
         }
     }
 }
--- TableControl/TableControl.ino
+++ TableControl/TableControl.ino
@@ -4,44 +4,69 @@
  Author:	Oren
 */
 
+#include <SoftwareSerial\SoftwareSerial.h>
+
 const int LED = 13;
 const unsigned long BAND = 9600;
 const int TRESHHOLD = 70;
 int photoValue = 0;
 
 String transmition;
+SoftwareSerial BT(10, 11);
 
+int trans;
 int result;
 
 // the setup function runs once when you press reset or power the board
 void setup() {
-	pinMode(13, OUTPUT);
-	Serial.begin(BAND);
+	pinMode(LED, OUTPUT);
+	digitalWrite(LED, LOW);
+	BT.begin(9600);
+
 }
 
 // the loop function runs over and over again until power down or reset
 void loop() {
 
-	//TODO: FOR MEGA CHANGE TO :
-	// while(Serial.available() > 0){
-	while (Serial.available() > 0) {
+	if (BT.available()) {
+		trans = BT.read();
+;
+		if (trans == 1) {
+			BT.println("LED ON");
+			digitalWrite(LED, HIGH);
+		}
 		
-		transmition += char(Serial.read());
+		if (trans == 2) {
+			digitalWrite(LED, LOW);
+		}
 	}
 
-	if (transmition == "") {
-		return;
+	else {
+		BT.println("NO INPUT");
 	}
+	////TODO: FOR MEGA CHANGE TO :
+	//// while(Serial.available() > 0){
+	//while (Serial.available() > 0) {
+	//	
+	//	transmition += char(Serial.read());
+	//	Serial.println(transmition);
+	//}
 
-	/*if(!transmition.equals(""))
-		Serial.println(transmition);*/
+	//if (transmition == "") {
+	//	return;
+	//}
 
-	if(transmition.equalsIgnoreCase("ON")){
-		digitalWrite(LED, HIGH);
-	}
+	///*if(!transmition.equals(""))
+	//	Serial.println(transmition);*/
 
-	if (transmition.equalsIgnoreCase("OFF")) {
-		digitalWrite(LED, LOW);
-	}
+	//if(transmition.equalsIgnoreCase("1")){
+	//	digitalWrite(LED, HIGH);
+	//	transmition = String("");
+	//}
+
+	//if (transmition.equalsIgnoreCase("0")) {
+	//	digitalWrite(LED, LOW);
+	//	transmition = String("");
+	//}
 
 }
